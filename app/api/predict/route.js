1ffad0773235b4f53326953b196c6c80c81260a1
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-10-11 23:37:27 -0700

    Feat: Route now predicts what is in image. (Note: Does not focus on food)

--- app/api/predict/route.js
+++ app/api/predict/route.js
@@ -1,13 +1,35 @@
 import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI()
+
 
 export async function POST(req) {
     const { image } = await req.json();
     console.log("Received image:", image);
 
     const base64Image = image.replace("data:image/jpeg;base64,", "");
-    console.log("\n\nBase64 image:", base64Image);
+    // console.log("\n\nBase64 image:", base64Image);
 
-    
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "user", 
+                content: [
+                    {type: "text", text: "What's in this image?"},
+                    {
+                        type: "image_url",
+                        image_url: {
+                            "url": image,
+                        },
+                    },
+                ],
+            },
+        ],
+        model: "gpt-4o-mini",
+    });
+
+    console.log(completion.choices[0]);
 
     return NextResponse.json({ message: "Image received" }, { status: 200 });
 }
