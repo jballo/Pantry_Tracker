17045d2de56b03954b516aab8eccf374885f2276
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-10-11 22:34:05 -0700

    Removed code related to google vertex ai.

--- app/api/predict/route.js
+++ app/api/predict/route.js
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import { PredictionServiceClient } from "@google-cloud/aiplatform";
 
 export async function POST(req) {
     const { image } = await req.json();
@@ -8,18 +7,7 @@ export async function POST(req) {
     const base64Image = image.replace("data:image/jpeg;base64,", "");
     console.log("\n\nBase64 image:", base64Image);
 
-    const clientOptions = {
-        apiEndpoint: `${process.env.VERTEX_AI_ENDPOINT_ID}-aiplatform.googleapis.com`,
-    };
-
-    const client = new PredictionServiceClient(clientOptions);
-
-    const request = {
-        instances: [{ image: base64Image }],
-    };
-
-    const [google_response] = await client.predict(request);
-    console.log("Google response:", google_response);
+    
 
     return NextResponse.json({ message: "Image received" }, { status: 200 });
 }
